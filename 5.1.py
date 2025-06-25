3f23da8a32a1af6e185b6aceece08d46c28f5d53
kzabi <kzabik12@gmail.com>
2025-06-25 20:28:59 +0300

    ok

--- 5.1.py
+++ 5.1.py
@@ -2,15 +2,15 @@ import keyword
 import string
 str_1 = str(input("Напишіть рядок: "))
 words = str_1.split("_")
-str_2 = str_1.replace("_", "")
-
 if any(x.isupper() for x in str_1):
     print("False")
 elif str_1[0].isdigit():
     print("False")
-elif any(c in string.punctuation for c in str_2):
+elif str_1 in keyword.kwlist:
     print("False")
-elif any(word in keyword.kwlist for word in words):
+elif any(c in string.punctuation for c in str_1.replace("_", "")):
+    print("False")
+elif set(str_1) <= {"_"} and str_1 != "_":
     print("False")
 else:
     print("True")
