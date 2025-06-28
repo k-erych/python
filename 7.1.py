63dc5bf17c8b66b4cad68bbaedfa4271e77521d7
kzabi <kzabik12@gmail.com>
2025-06-28 13:45:48 +0300

    ok

---
+++ 7.1.py
@@ -0,0 +1,5 @@
+def say_hi(name, age):
+    print(f'Hi. My name is {name} and I am {age} years old')
+name_1 = str(input("Введіть ваше ім'я: "))
+age_1 = int(input("Введіть ваш вік: "))
+say_hi(name_1, age_1)
