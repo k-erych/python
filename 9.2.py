47f1654be9b4931825ebb0d5b220be427f5d804c
kzabi <kzabik12@gmail.com>
2025-07-04 13:34:54 +0300

    ok

--- 9.2.py
+++ 9.2.py
@@ -1,10 +1,17 @@
 def difference(*args):
     list_1 = []
+    a = 0
     for arg in args:
-        arg = float(arg)
-        list_1.append(arg)
-    a = max(list_1) - min(list_1)
-    if a % 1 == 0:
-        a = int(a)
+        if arg + arg != 0:
+            for arg in args:
+                arg = float(arg)
+                list_1.append(arg)
+            a = max(list_1) - min(list_1)
+            if a % 1 == 0:
+                a = int(a)
+            else:
+                a = round(a, 2)
+        else:
+            a = 0
     return a
-print(difference(5, 6, 2, 9, 5.2))
+print(difference(10.2, -2.2, 0, 1.1, 0.5))
